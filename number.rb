8d1e9e65ee73d21ca8fa2709250b64f1ee167292
Geovana <geovana.souza@willbank.com>
2021-03-16 09:08:46 -0300

    Operaçoes matematicas

---
+++ number.rb
@@ -0,0 +1,26 @@
+x = 5.0
+y = 3
+
+puts "Valor de x: #{x}"
+puts "Valor de y: #{y}"
+puts ""
+
+# addition
+sum = x + y
+puts "Soma: #{sum}"
+
+#subtraction
+sub = x - y
+puts "Subtração: #{sub}"
+
+#multiplication
+mul = x * y
+puts "Multiplicação: #{mul}"
+
+# division
+div = x / y
+puts "Divisão: #{div}"
+
+# mod
+mod = x % y
+puts "Mod: #{mod}"
