f6b3267a290e9e8e63c29be99e1d194733b0d647
Geovana <geovana.souza@willbank.com>
2021-03-16 09:14:28 -0300

    Exemplo erro concatenaçao string + number

--- strings.rb
+++ strings.rb
@@ -8,3 +8,8 @@ string_1 = "Hello "
 string_2 = 'World'
 puts string_1 + string_2
 puts string_1 + ", " + string_2
+
+# Forçando erro: concatenação de string com número
+#x = 5
+#puts string_1 + x
+# Causa erro no implicit conversion of Integer into String (TypeError)
