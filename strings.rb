7a493a4f0d4753b4cd7eefdbc1a3db222acff374
Geovana <geovana.souza@willbank.com>
2021-03-16 09:13:12 -0300

    Interpolaçao e Concatenaçao de Strings

---
+++ strings.rb
@@ -0,0 +1,10 @@
+# Interpolação de Strings
+name = "Jonathan"
+age = 25
+puts "Your name is #{name} and you are #{age} years old"
+
+# Concatenação de strings
+string_1 = "Hello "
+string_2 = 'World'
+puts string_1 + string_2
+puts string_1 + ", " + string_2
