22e15cac5eecef1eca646c19a25b4ce47bbc1759
Geovana <geovana.souza@willbank.com>
2021-03-16 08:53:21 -0300

    Imprimindo string + variaveis

--- variables.rb
+++ variables.rb
@@ -1,8 +1,8 @@
 # output the value of the variable myVar
 my_var = 5
-puts my_var
+puts "O valor da variável my_var é #{my_var}"
 
 x = 5
 y = 15
 sum = x + y
-puts sum
+puts "A soma de #{x} + #{y} é #{sum}"
