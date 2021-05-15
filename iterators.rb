0057594262ff048cf0be1eb0fa889790da2b3438
personal <geovana.possenti@gmail.com>
2021-05-15 15:12:04 -0300

    Add iterator examples

---
+++ iterators.rb
@@ -0,0 +1,9 @@
+# Iterator
+5.times { puts "I am Learning Ruby!" }
+
+puts ""
+
+# Each iterator
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+# Interact wich each item, store the value in day variable and print on the screen
+days.each { |day| puts day }
