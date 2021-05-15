6c3a4e75be2ea2d8671a5a5d0b451697739c7f3c
personal <geovana.possenti@gmail.com>
2021-05-15 15:14:57 -0300

    Add block examples

--- iterators.rb
+++ iterators.rb
@@ -7,3 +7,10 @@ puts ""
 days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 # Interact wich each item, store the value in day variable and print on the screen
 days.each { |day| puts day }
+
+puts ""
+
+# Blocks + Iterator
+1.times do
+    puts "This is a code block!"
+end
