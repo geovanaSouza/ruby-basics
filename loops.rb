43a277909276aed94ea073a86e3bd5ffc19cb2aa
Geovana Possenti <geovana.possenti@ifood.com.br>
2021-05-15 14:41:26 -0300

    Add loops example

---
+++ loops.rb
@@ -0,0 +1,24 @@
+# while loop
+count = 1
+
+while count < 10
+    puts count
+    count = count + 1
+end
+
+puts ""
+
+# false loop
+count = 6
+
+until count > 10
+    puts count
+    count = count + 1
+end
+
+puts ""
+
+# For loop
+for count in 1...10
+    puts count
+end
